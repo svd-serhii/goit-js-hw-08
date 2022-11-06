e59d1e4ec6cc13d3d5387e98fbcc0810d392a8f3
Serhii Demchenko <svdemchenko@gmail.com>
2022-11-06 11:50:44 +0200

    add task#1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryItemMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemMarkup);
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+galleryEl.addEventListener('click', onGalleryElClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+            />
+        </a>`;
+    })
+    .join('');
+}
+
+function onGalleryElClick(e) {
+  e.preventDefault();
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// console.log(galleryItems);
