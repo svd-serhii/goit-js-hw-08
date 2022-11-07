43233ab1dcbe1cd0cf4cd0d67404008f79413abf
Serhii Demchenko <svdemchenko@gmail.com>
2022-11-07 18:14:14 +0200

    add task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.addEventListener('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {};
