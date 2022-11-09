42e0390025f9863749e30b8f6124a4b86fe280d8
Serhii Demchenko <svdemchenko@gmail.com>
2022-11-09 21:00:24 +0200

    upd task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,25 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.addEventListener('iframe');
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = function (data) {};
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+  )
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
